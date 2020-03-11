97c24b15c07b617e766733d50afb87dba9a0bf50
Adam Johnston <ajohnston1219@gmail.com>
2020-03-11 17:44:58 -0500

    setup build system

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,17 @@
+///////////////////////////
+// Handmade Hero Game    //
+// Author: Adam Johnston //
+// Created: 3/12/2020    //
+///////////////////////////
+
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    PWSTR     lpCmdLine,
+                    int       nCmdShow)
+{
+    MessageBoxA(0, "Hey, fucker!", "FUCK YOU",
+                MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
