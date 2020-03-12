98c1be1443c4ff7ba4d61b97b1a320b584c5df77
Adam Johnston <ajohnston1219@gmail.com>
2020-03-11 20:12:59 -0500

    created (epilepsy) window

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -6,12 +6,132 @@
 
 #include <windows.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance,
-                    HINSTANCE hPrevInstance,
-                    PWSTR     lpCmdLine,
-                    int       nCmdShow)
+LRESULT CALLBACK MainWindowCallback(HWND   Window,
+                                    UINT   Message,
+                                    WPARAM WParam,
+                                    LPARAM LParam)
 {
-    MessageBoxA(0, "Hey, fucker!", "FUCK YOU",
-                MB_OK|MB_ICONINFORMATION);
+    LRESULT Result = 0;
+
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            static DWORD Operation = WHITENESS;
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if (Operation == WHITENESS)
+            {
+                Operation = BLACKNESS;
+            }
+            else
+            {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+            // OutputDebugStreamA("default\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return Result;
+}
+
+int WINAPI wWinMain(HINSTANCE Instance,
+                    HINSTANCE PrevInstance,
+                    PWSTR     CommandLine,
+                    int       ShowCode)
+{
+    /*
+     * Define window class
+     */
+    WNDCLASSA WindowClass = {};
+    // TODO(adam): Check if HREDRAW/VREDRAW/OWNDC still matter
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    // WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    /*
+     * Register our window class
+     */
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle =
+            CreateWindowExA(
+                0,
+                WindowClass.lpszClassName,
+                "Handmade Hero",
+                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                Instance,
+                0);
+        /*
+         * If successful, loop through messages
+         */
+        if (WindowHandle)
+        {
+            MSG Message;
+            for(;;)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        // Failed to create window
+        else
+        {
+            // TODO(adam): Logging
+        }
+    }
+    // Failed to register window class
+    else
+    {
+        // TODO(adam): Logging
+    }
+
     return 0;
 }
