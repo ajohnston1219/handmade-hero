aa453c5db563b5e992639660b699363f262800e2
Adam Johnston <ajohnston1219@gmail.com>
2020-03-17 05:23:47 -0500

    added sprintf warning back and now using _snprintf_s instead

--- handmade/code/build.bat
+++ handmade/code/build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4996 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -FC -Z7 -Fmwin32_handmade.map 
+set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -FC -Z7 -Fmwin32_handmade.map
 
-set CommonLinkerFlags=-opt:ref user32.lib Gdi32.lib
+set CommonLinkerFlags=-opt:ref user32.lib Gdi32.lib winmm.lib
 :: TODO - can we just build both with one exe?
 
 IF NOT EXIST ..\..\build mkdir ..\..\build
